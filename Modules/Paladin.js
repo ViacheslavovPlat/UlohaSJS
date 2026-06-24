64a15fe52dbfd34047c6ec440e6b7c8ebf4be7ac
Platon <viacheslavovplaton@.gmail.com>
2026-06-24 12:27:38 +0200

    Pladin up to data

--- Modules/Paladin.js
+++ Modules/Paladin.js
@@ -1,8 +1,19 @@
 function Paladin(name){
-    Warrior.call(name);
+    Warrior.call(this, name);
 
-    
+    this.mana = 30;
+    this.attackPower = 15; 
+    this.baseXp = 15;
+    this.totalXp = this.baseXp;
 }
 
 Paladin.prototype = Object.create(Warrior.prototype);
-Paladin.prototype.constructor = Warrior;
+Paladin.prototype.constructor = Paladin;
+
+Paladin.prototype.attack = function(target, callback){
+    let holyDamage = Math.floor(this.mana / 10);
+    
+    target.takeDamage(holyDamage);
+    
+    Warrior.prototype.attack.call(this, target, callback);
+}
